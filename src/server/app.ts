71c48edfb7565ed3b80e961e7a382dd3c5c55764
Alex Uribe <repzyyog@gmail.com>
2025-04-15 11:55:52 +0200

    Create PokemonController class with types

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import checkHealthStatus from "./middlewares/checkHealthStatus.js";
+import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
 
 const app = express();
 
+app.use(cors({ origin: "https://pokemon-alex.netlify.app/" }));
+
 app.get("/", checkHealthStatus);
+
+app.use(handleEndpointNotFound);
 
 export default app;
