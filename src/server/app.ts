43fcc98f61ba07bbe40b51b85d472c542667a119
Alex Uribe <repzyyog@gmail.com>
2025-04-15 11:14:51 +0200

    Create checkHealthStatus endpoint

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import checkHealthStatus from "./middlewares/checkHealthStatus.js";
 
 const app = express();
+
+app.get("/", checkHealthStatus);
 
 export default app;
