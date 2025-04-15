a828d3a4764ad07a2ac5d08eb0b1a3d38481c967
Alex Uribe <repzyyog@gmail.com>
2025-04-15 13:45:34 +0200

    Create pokemonsRouter with GET/pokemon

---
+++ src/pokemon/router/pokemonsRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import PokemonController from "../controller/PokemonController.js";
+import { pokemons } from "../data/pokemons.js";
+
+const pokemonsRouter = Router();
+
+const pokemonController = new PokemonController(pokemons);
+
+pokemonsRouter.get("/", pokemonController.getPokemons);
+
+export default pokemonsRouter;
