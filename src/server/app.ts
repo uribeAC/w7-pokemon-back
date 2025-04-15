db7146cd56bef839c8155d53d89923f3778237b1
Alex Uribe <repzyyog@gmail.com>
2025-04-15 13:46:19 +0200

    Add pokemonRouter middleware and morgan

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import checkHealthStatus from "./middlewares/checkHealthStatus.js";
 import handleEndpointNotFound from "./middlewares/handleEndpointNotFound.js";
+import pokemonsRouter from "../pokemon/router/pokemonsRouter.js";
 
 const app = express();
 
 app.use(cors({ origin: "https://pokemon-alex.netlify.app/" }));
 
+app.use(morgan("dev"));
+
 app.get("/", checkHealthStatus);
+
+app.use("/pokemon", pokemonsRouter);
 
 app.use(handleEndpointNotFound);
 
