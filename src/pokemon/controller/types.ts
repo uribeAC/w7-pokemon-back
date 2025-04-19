ee19ff4d4c7f9657f2e53c8f7ff4623165d6e352
Alex Uribe <repzyyog@gmail.com>
2025-04-19 11:56:33 +0200

    Add getPokemon method to PokemonController

--- src/pokemon/controller/types.ts
+++ src/pokemon/controller/types.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export interface PokemonControllerStructure {
   getPokemons: (req: Request, res: Response) => void;
+  getPokemon: (req: Request, res: Response) => void;
   addPokemon: (req: Request, res: Response) => void;
   deletePokemon: (req: Request, res: Response) => void;
   addToPokeball: (req: Request, res: Response) => void;
